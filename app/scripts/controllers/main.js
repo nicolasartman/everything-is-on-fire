49741abc3b7b6eed0fee11885ced606a230354c7
Nicolas Artman <nicolas@udacity.com>
2013-11-16 02:27:54 -0800

    add base tooling

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('everythingIsOnFireApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
