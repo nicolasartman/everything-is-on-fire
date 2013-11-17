ba3d7b2aeae0ca63a25f0b8ea8942b67c5f83a47
Nicolas Artman <nicolas@udacity.com>
2013-11-16 16:51:54 -0800

    add silly demo ui interaction

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -3,8 +3,36 @@
 
 angular.module('everythingIsOnFireApp')
 
-.controller('main', ['$scope', 'game', function ($scope, game) {
+.controller('main',
+	['$scope', 'game', '$timeout',
+	function ($scope, game, $timeout) {
 	'use strict';
 
 	$scope.test = game.getThing();
+	
+	$scope.lasersCharged = false;
+	
+	$timeout(function () {
+		$scope.timeLeftToAct = 5;
+		
+		var countdown = window.setInterval(function () {
+			$scope.timeLeftToAct -= 1;
+			$scope.obstacle = 'cow';
+			
+			if ($scope.timeLeftToAct <= 0 || $scope.lasersCharged) {
+				window.clearInterval(countdown);
+				$scope.obstacle = null;
+				$scope.lasersCharged = false;
+			}
+			
+			$scope.$apply();
+		}, 1000);
+	}, 2000);
+	
+	$scope.chargeLasers = function () {
+		$scope.lasersCharged = true;
+		if (!$scope.obstacle) {
+			$scope.lasersCharged = false;
+		}
+	}
 }]);
