033aced65cb5eea0660763fe12189966b710d131
Kelly Wong <kelly@bluejeans.com>
2013-11-16 19:41:36 -0800

    Add basic getters for shared data

--- app/scripts/services/game.js
+++ app/scripts/services/game.js
@@ -1,28 +1,70 @@
 angular.module('everythingIsOnFireApp').factory('game', function () {
 	'use strict';
 	
-	var gameData = {}, self = {};
-	
-	var mappedThings = _.map([1,2,3], function (item) {
-		return item * 2;
-	});
-	
-	console.log(mappedThings);
+	var gameData = {
+                        "robotOneHealth": 1000000,
+                        "robotOneHits": 0,
+                        "robotOneTasks": {},
+
+                        "robotTwoHealth": 1000000,
+                        "robotTwoHits": 0,
+                        "robotTwoTasks": {}
+                       };
+        var self = {};
 	
 	var root = new Firebase('https://everythingisonfire.firebaseIO.com/');
-	
-	// Whenever the page loads, bump the test value
-	var testRef = root.child('test');
-	testRef.transaction(function (current) {
-		return (current || 0) + 1;
-	});
-	
-	
-	// Expose state getters here
-	
-	self.getThing = function () {
-		return gameData.thing;
-	}
-	
+        var robotOne = root.child('robotOne');
+        var robotOneActionQueue = robotOne.child('activeEvents');
+        var robotOneHealth = robotOne.child('health');
+        var robotOneHits = robotOne.child('hits');
+
+        var robotTwo = root.child('robotTwo');
+        var robotTwoActionQueue = robotTwo.child('activeEvents');
+        var robotTwoHealth = robotTwo.child('health');
+        var robotTwoHits = robotTwo.child('hits');
+
+        self.reset = function() {
+            robotOneHealth.set(1000000000);
+            robotOneActionQueue.set(null);
+            robotOneHits.set(0);
+
+            robotTwoHealth.set(1000000000);
+            robotTwoActionQueue.set(null);
+            robotTwoHits.set(0);
+        };
+
+	robotOneActionQueue.on('child_removed', function(snapshot) {
+            console.log('task removed for robot 1');
+            console.log(snapshot.val());
+            gameData["robotOneTasks"][snapshot.name()] = snapshot.val();
+        });
+	robotOneActionQueue.on('child_added', function(snapshot) {
+            console.log('New task for robot 1');
+            console.log(snapshot.val());
+            gameData["robotOneTasks"][snapshot.name()] = snapshot.val();
+        });
+
+        self.hitRobotOne = function(hits) {
+            robotOneHits.transaction(function (current) {
+		return (current || 0) + hits;
+            });
+	};
+
+	robotOneActionQueue.on('child_removed', function(snapshot) {
+            console.log('task removed for robot 1');
+            console.log(snapshot.val());
+            gameData["robotTwoTasks"][snapshot.name()] = snapshot.val();
+        });
+	robotTwoActionQueue.on('child_added', function(snapshot) {
+            console.log('New task for robot 2');
+            console.log(snapshot.val());
+            gameData["robotTwoTasks"].push(snapshot.val());
+        });
+        self.hitRobotTwo = function(hits) {
+            robotTwoHits.transaction(function (current) {
+		return (current || 0) + hits;
+            });
+	};
+
 	return self;
 });
